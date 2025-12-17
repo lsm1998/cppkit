4f24eab5708a0b4bfeac91a77820c045e6c3ada4
lsm <487005831@qq.com>
2025-12-17 21:17:42 +0800

    ProcessManager

--- include/cppkit/fmt.hpp
+++ include/cppkit/fmt.hpp
@@ -3,118 +3,140 @@
 #include <iostream>
 #include <sstream>
 
-namespace cppkit::inner {
-constexpr size_t count_placeholders(const char *fmt, const size_t len) {
-  size_t count = 0;
-  for (size_t i = 0; i + 1 < len; ++i) {
-    if (fmt[i] == '{' && fmt[i + 1] == '{') {
-      // 转义 "{{" -> 输出 '{'，跳过
-      ++i;
-      continue;
+namespace cppkit::inner
+{
+    constexpr size_t count_placeholders(const char* fmt, const size_t len)
+    {
+        size_t count = 0;
+        for (size_t i = 0; i + 1 < len; ++i)
+        {
+            if (fmt[i] == '{' && fmt[i + 1] == '{')
+            {
+                // 转义 "{{" -> 输出 '{'，跳过
+                ++i;
+                continue;
+            }
+            if (fmt[i] == '}' && fmt[i + 1] == '}')
+            {
+                // 转义 "}}" -> 输出 '}'，跳过
+                ++i;
+                continue;
+            }
+            if (fmt[i] == '{' && fmt[i + 1] == '}')
+            {
+                ++count;
+                ++i;
+            }
+        }
+        return count;
     }
-    if (fmt[i] == '}' && fmt[i + 1] == '}') {
-      // 转义 "}}" -> 输出 '}'，跳过
-      ++i;
-      continue;
+
+    template <size_t N>
+    struct FormatString
+    {
+        char str[N]{};
+        static constexpr size_t length = N - 1;
+
+        constexpr FormatString(const char (&s)[N])
+        {
+            for (size_t i = 0; i < N; ++i)
+                str[i] = s[i];
+        }
+
+        [[nodiscard]] constexpr size_t get_placeholder_count() const
+        {
+            return count_placeholders(str, N);
+        }
+    };
+
+    inline void format_impl(std::ostringstream& oss, const char* fmt)
+    {
+        while (*fmt)
+        {
+            if (fmt[0] == '{' && fmt[1] == '{')
+            {
+                oss << '{';
+                fmt += 2;
+                continue;
+            }
+            if (fmt[0] == '}' && fmt[1] == '}')
+            {
+                oss << '}';
+                fmt += 2;
+                continue;
+            }
+            oss << *fmt++;
+        }
     }
-    if (fmt[i] == '{' && fmt[i + 1] == '}') {
-      ++count;
-      ++i;
+
+    template <typename T, typename... Args>
+    void format_impl(std::ostringstream& oss, const char* fmt, const T& arg,
+                     const Args&... args)
+    {
+        while (*fmt)
+        {
+            if (fmt[0] == '{' && fmt[1] == '{')
+            {
+                oss << '{';
+                fmt += 2;
+                continue;
+            }
+            if (fmt[0] == '}' && fmt[1] == '}')
+            {
+                oss << '}';
+                fmt += 2;
+                continue;
+            }
+            if (fmt[0] == '{' && fmt[1] == '}')
+            {
+                oss << arg;
+                format_impl(oss, fmt + 2, args...);
+                return;
+            }
+            oss << *fmt++;
+        }
     }
-  }
-  return count;
-}
 
-template <size_t N> struct FormatString {
-  char str[N]{};
-  static constexpr size_t length = N - 1;
+    template <FormatString Fmt, typename... Args>
+    std::string safe_fmt_sprintf_impl(const Args&... args)
+    {
+        constexpr size_t expected = Fmt.get_placeholder_count();
+        constexpr size_t actual = sizeof...(Args);
 
-  constexpr FormatString(const char (&s)[N]) {
-    for (size_t i = 0; i < N; ++i)
-      str[i] = s[i];
-  }
+        static_assert(expected == actual,
+                      "Number of placeholders does not match number of arguments");
 
-  [[nodiscard]] constexpr size_t get_placeholder_count() const {
-    return count_placeholders(str, N);
-  }
-};
-
-inline void format_impl(std::ostringstream &oss, const char *fmt) {
-  while (*fmt) {
-    if (fmt[0] == '{' && fmt[1] == '{') {
-      oss << '{';
-      fmt += 2;
-      continue;
+        std::ostringstream oss;
+        format_impl(oss, Fmt.str, args...);
+        return oss.str();
     }
-    if (fmt[0] == '}' && fmt[1] == '}') {
-      oss << '}';
-      fmt += 2;
-      continue;
+
+    template <FormatString Fmt, typename... Args>
+    void safe_fmt_print_impl(const Args&... args)
+    {
+        std::cout << safe_fmt_sprintf_impl<Fmt>(args...) << std::endl;
     }
-    oss << *fmt++;
-  }
-}
 
-template <typename T, typename... Args>
-void format_impl(std::ostringstream &oss, const char *fmt, const T &arg,
-                 const Args &...args) {
-  while (*fmt) {
-    if (fmt[0] == '{' && fmt[1] == '{') {
-      oss << '{';
-      fmt += 2;
-      continue;
+    template <typename... Args>
+    std::string runtime_fmt_sprintf_impl(const char* fmt, const Args&... args)
+    {
+        std::ostringstream oss;
+        format_impl(oss, fmt, args...);
+        return oss.str();
     }
-    if (fmt[0] == '}' && fmt[1] == '}') {
-      oss << '}';
-      fmt += 2;
-      continue;
-    }
-    if (fmt[0] == '{' && fmt[1] == '}') {
-      oss << arg;
-      format_impl(oss, fmt + 2, args...);
-      return;
-    }
-    oss << *fmt++;
-  }
-}
-
-template <FormatString Fmt, typename... Args>
-std::string safe_fmt_sprintf_impl(const Args &...args) {
-  constexpr size_t expected = Fmt.get_placeholder_count();
-  constexpr size_t actual = sizeof...(Args);
-
-  static_assert(expected == actual,
-                "Number of placeholders does not match number of arguments");
-
-  std::ostringstream oss;
-  format_impl(oss, Fmt.str, args...);
-  return oss.str();
-}
-
-template <FormatString Fmt, typename... Args>
-void safe_fmt_print_impl(const Args &...args) {
-  std::cout << safe_fmt_sprintf_impl<Fmt>(args...) << std::endl;
-}
-
-template <typename... Args>
-std::string runtime_fmt_sprintf_impl(const char* fmt, const Args &...args) {
-  std::ostringstream oss;
-  format_impl(oss, fmt, args...);
-  return oss.str();
-}
 } // namespace cppkit::inner
 
 
-namespace cppkit{
-  template <typename... Args>
-  std::string format(const char* fmt, Args&&... args) {
-    return cppkit::inner::runtime_fmt_sprintf_impl(fmt, std::forward<Args>(args)...);
-  }
+namespace cppkit
+{
+    template <typename... Args>
+    std::string format(const char* fmt, Args&&... args)
+    {
+        return cppkit::inner::runtime_fmt_sprintf_impl(fmt, std::forward<Args>(args)...);
+    }
 }
 
 
 #define print(fmt_str, ...) inner::safe_fmt_print_impl<fmt_str>(__VA_ARGS__)
 
 #define sprintf(fmt_str, ...) inner::safe_fmt_sprintf_impl<fmt_str>(__VA_ARGS__)
-
-
--- tests/process_test.cpp
+++ tests/process_test.cpp
@@ -8,13 +8,12 @@ int main()
     // 创建管理器实例
     cppkit::ProcessManager pm;
 
-    pm.spawn("ping", {"-c", "3", "127.0.0.1"}, [](pid_t pid, const int code)
+    pm.spawn("ping", {"-c", "3", "127.0.0.1"}, [](pid_t _, const int code)
     {
         if (code == 0)
             cppkit::sync_stream(std::cout) << "ping命令执行完成\n";
         else
             cppkit::sync_stream(std::cout) << "ping执行失败 " << code << ".\n";
-
     });
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -24,7 +23,7 @@ int main()
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     // 测试杀死进程
-    const pid_t sleepPid = pm.spawn("sleep", {"10"}, [](pid_t pid, const int code)
+    const pid_t sleepPid = pm.spawn("sleep", {"10"}, [](pid_t _, const int code)
     {
         // 被信号杀死的进程，code 通常是负数 (例如 -15 SIGTERM 或 -9 SIGKILL)
         cppkit::sync_stream(std::cout) << "sleep 执行完成 code: " << code << "\n";
@@ -36,8 +35,9 @@ int main()
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     // 测试长时间运行的进程
-    pm.spawn("sleep", {"50"}, [](pid_t pid, int code)
+    pm.spawn("sleep", {"50"}, [](pid_t _, const int code)
     {
+        cppkit::sync_stream(std::cout) << "长时间 sleep 进程结束，code: " << code << "\n";
     });
 
     std::this_thread::sleep_for(std::chrono::seconds(2));
